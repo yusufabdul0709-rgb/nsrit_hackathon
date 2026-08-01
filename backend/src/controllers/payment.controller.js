8f19945b7ef2e3de73382682df87f3aa8f458645
Yunusr714 <yusufabdul0709@gmail.com>
2026-08-01 09:23:13 +0530

    feat: initialize wallet routes and stub payment and ticket controller methods

---
+++ backend/src/controllers/payment.controller.js
@@ -0,0 +1,6 @@
+exports.initiatePayment = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.verifyPayment = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.recordOfflinePayment = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.syncOfflineQueue = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.createRazorpayOrder = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.verifyTopUp = async (req, res) => res.status(501).json({ message: "Not implemented" });
---
+++ backend/src/controllers/ticket.controller.js
@@ -0,0 +1,3 @@
+exports.createTicket = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.getTicketHistory = async (req, res) => res.status(501).json({ message: "Not implemented" });
+exports.getTicketById = async (req, res) => res.status(501).json({ message: "Not implemented" });
